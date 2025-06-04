d1dfce40767cde499d4812ea9bac31e37f121d4b
JohnSisk4 <151465587+JohnSisk4@users.noreply.github.com>
2025-06-04 17:07:38 +0100

    Add project files.

---
+++ Favorite Music Video - Final Assignment/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿document.getElementById("tiltButton").addEventListener("click", function () {
+    let video = document.getElementById("video");
+
+    // Check the current rotation
+    if (video.style.transform === "rotate(10deg)") {
+        video.style.transform = "rotate(0deg)"; // Reset position
+    } else {
+        video.style.transform = "rotate(10deg)"; // Tilt position
+    }
+});
+
+
+
